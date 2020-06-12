289a7cc8ba9e5050b2f06eab267da6ee74061a29
Dean Bernocchi <dean.bernocchi@gmail.com>
2020-06-12 00:06:27 -0500

    Refactored code

--- static/js/app.js
+++ static/js/app.js
@@ -1,14 +1,17 @@
 // import the data from data.js
 const tableData = data;
 
-// Clean the data
+// *********************************************
+// ******         Table Data         ***********
+// ******       Cleaning Section     ***********
+// *********************************************
+
+// ***** Change the case of data fieldsc *****
 // Capitalize city name and multipart city names
 // States to upper case
 // Counties to upper case
-
-
-data.forEach((dataRow) => {
-
+// using jscript for ... of ...
+for (dataRow of data ) {
     var cityName = "";
 
     dataRow["city"].split(" ").forEach((cityNamePart) => {
@@ -20,20 +23,23 @@ data.forEach((dataRow) => {
     dataRow["city"]    = cityName;
     dataRow["state"]   = dataRow["state"].toUpperCase();
     dataRow["country"] = dataRow["country"].toUpperCase();
-});
+};
 
 
-// *********************************************
-// ******  Get unique field values   ***********
-// ****** from the data array object ***********
-// *********************************************
+// ***** Get a unique set of value for each  *****
+// ***** data field used to filter data rows *****
 
-// Create a data field values object that has
-// properties which contains sets collections.
-// Populating these data field set collections
-// generates unique set of values for each
-// data field
-dataFieldValues = {
+// Createa a data field unique sorted values object
+// This object will contain the unique sorted values
+// for each data field used to filter data rows.
+
+// A javascript set objects is used to hold a data field's
+// unique values. The set object itself will create the 
+// the data field's unique value list. A set object will  
+// only retain value once. If the same value is added to 
+// the set multiple time, the set object will only keep
+// the first instance of the value. 
+uniqSortFldVals = {
     datetime: new Set(),
     city:     new Set(),
     state:    new Set(),
@@ -41,15 +47,32 @@ dataFieldValues = {
     shape:    new Set()
 };
 
-// Populate data field set collections in the 
-// the data field values object
-data.forEach((dataRow) => {
-    dataFieldValues.datetime.add(dataRow["datetime"]);
-    dataFieldValues.city.add(dataRow["city"]);
-    dataFieldValues.state.add(dataRow["state"]);
-    dataFieldValues.country.add(dataRow["country"]);
-    dataFieldValues.shape.add(dataRow["shape"]);
+
+// Populate the uniqSortFldVals object with 
+// table data from the tableData object
+tableData.forEach((dataRow) => {
+
+    // For each column name in the data row
+    // Using the javascript statement for ... in ...
+    for ( columnName in dataRow ){
+
+        // If uniqSortFldVals does not have the property
+        // with a name equal to the columnName value
+        // continue.
+        if (uniqSortFldVals[columnName] === undefined){
+            continue;
+        }
+
+        // Add value of the field to the uniqSortFldVals
+        // property whose name is equal to the columnName value
+        uniqSortFldVals[columnName].add(dataRow[columnName]);
+    }
+
 });
+
+
+// ***** Sort unique set of value for each   *****
+// ***** data field used to filter data rows *****
 
 // Date mm/dd/yyyy regular expression
 // which is used to determine if the data field
@@ -57,10 +80,12 @@ data.forEach((dataRow) => {
 var datetimeRegExp = new RegExp("\\d{1,2}/\\d{1,2}/\\d{4}")
 
 
-// Sort the unique data field values
-Object.values(dataFieldValues).forEach((dataField) => {
+// Sort the unique data value for each data field
+for (dataFieldName in uniqSortFldVals) {
 
-    var arrayToSort = Array.from(dataField).sort();
+    let dataFldUnqValSet = uniqSortFldVals[dataFieldName];
+
+    var arrayToSort = Array.from(dataFldUnqValSet).sort();
 
     // Test to see if the array contains date string
     // Date strings must be sorted using a custome
@@ -92,72 +117,166 @@ Object.values(dataFieldValues).forEach((dataField) => {
         dateArrayToSort.forEach((value) => { arrayToSort.push((new Date(value)).toLocaleDateString('en-US')) })
     };
 
-    // Clear all values in 
-    // data field unique value set
-    dataField.clear();
+    // Clear all values in  
+    //data field unique value set
+    dataFldUnqValSet.clear();
 
     // Repopulate the data field unique value 
-    // set with the sorted values 
-    arrayToSort.forEach((value) => {dataField.add(value)});
-});
+    // set with the values in sorted order 
+    arrayToSort.forEach((value) => {dataFldUnqValSet.add(value)});
+};
 
 
 // *********************************************
-// ******  Functions to Manage the   ***********
-// ******     Form Select Boxes      ***********
+// ******            END             ***********
+// ******         Table Data         ***********
+// ******       Cleaning Section     ***********
 // *********************************************
 
-// Function to populate form select box options
-function populateSelectOptions(selectId, selectOptionData, format="none"){
-    
-    // Get all select control by id
-    var select = d3.select("#" + selectId)
 
-    // Set the function to handle
-    // select control onChange events
-    select.on("change", selectOnChange);
+// *********************************************
+// ******    Code to Manage the      ***********
+// ******  Filter Select Elements    ***********
+// *********************************************
 
-    //Add the option for the select control
-    //from the sorted unique value data field set
-    selectOptionData.forEach((value) => {select.append("option").attr("value", value).text(value);});
+class UFOTableFilter {
+
+    constructor() {
+        this.datetime  = new Array();
+        this.city      = new Array();
+        this.state     = new Array();
+        this.country   = new Array();
+        this.shape     = new Array();
+    }
+
+    filter(dataObject){ 
+
+        if (this.isEmpty() === true){
+            return true;
+        }
+
+        let datetimeFilter = (this.datetime.length === 0 || this.datetime.includes(dataObject.datetime));
+
+        let cityFilter     = (this.city.length === 0 || this.city.includes(dataObject.city));
+
+        let stateFilter    = (this.state.length === 0 || this.state.includes(dataObject.state));
+
+        let countryFilter  = (this.country.length === 0 || this.country.includes(dataObject.country));
+
+        let shapeFilter    = (this.shape.length === 0 || this.shape.includes(dataObject.shape));
+
+        let filterReturn   = datetimeFilter && cityFilter && stateFilter && countryFilter && shapeFilter;
+        
+        return filterReturn;
+    }
+
+    setFilter(dataFieldName, filterDataArray){
+        this[dataFieldName] = filterDataArray;
+    }
+
+    clear(){
+        this.datetiime  = new Array();
+        this.city       = new Array();
+        this.state      = new Array();
+        this.country    = new Array();
+        this.shape      = new Array();
+    }
+ 
+    isEmpty() {
+        return this.datetime.length + this.city.length + this.state.length + this.country.length + this.shape.length === 0
+    }
 }
 
-function getSelectOptions(id) {
+// Create and instance of a UFOTableFilterObject
+let ufoTableFilter = new UFOTableFilter();
 
-    let optionArray = new Array();
+// Function to populate all filter select element
+function populateFilterSelects(optionValues, onChangeEventHandler) {
     
-    // Find the select box by the id attribute
-    // and get the values from the selected options
-    Object.values(d3.select("#" + id)
-    .property("options"))
-    .forEach((option) => { option.selected === true&option.value.length > 0?optionArray.push(option.value):'' });
+    // Get an array of all the filter select elements
+    let d3SelectCollection = d3.selectAll("select");
 
-    return optionArray;
+    // Set the onchange event handler for all filter select elements
+    d3SelectCollection.on("change", onChangeEventHandler);
+
+    let selectelementArray = d3SelectCollection._groups[0]
+
+    // Interate through each select element's option objects
+    // and set the option object's selected property to false
+
+    let selectOptionData;
+    let d3Select;
+
+    for (select of selectelementArray){
+
+        // Get the unique sorted field data to 
+        // populate the filter select element
+        selectOptionData = optionValues[select.id];
+        
+        // Doing it using d3 way
+        d3Select = d3.select("#" + select.id);
+
+        selectOptionData.forEach((value) => {d3Select.append("option").attr("value", value).text(value)});
+        
+        // Doing it the DOM way (Just having some fun. Use to be front end developer years ago.)
+        // selectOptionData.forEach((value) =>
+        // { 
+        //     let selectOption    = document.createElement("option");
+        //     selectOption.value  = value;
+        //     selectOption.text   = value;
+        //     select.add(selectOption);
+        // });
+        
+    }
 }
 
 
-// Function to unset selected options
-// in the filter select controls
-function clearSelectBoxes() {
-
+// function to clear the filter select elements
+function clearFilterSelects() {
     
-    let selection = d3.selectAll("select");
+    // Get an array of all the filter select elements
+    let selectelementArray = d3.selectAll("select")._groups[0];
 
-    Object.values(selection._groups).forEach((selectArray) => {
-        // Find the select box by the id attribute
-        // and interate through the select box options
-        // setting selected property to false
-        selectArray.forEach((select) => { Object.values(select.options).forEach((option) => { option.selected = false;}) })
-    });
+    // Interate through each select element's option objects
+    // and set the option object's selected property to false
+    selectelementArray.forEach((select) => 
+        {
+            Object.values(select.options).forEach((option) => 
+            { 
+                option.selected = false;} 
+            )
+        }
+    );
 
+    ufoTableFilter.clear();
+
+    // The filter has been cleared.
+    // Populate the table with all data.
     buildTable(tableData);
 }
 
+function updateFilter() {
+    
+    let filterValues = new Array();
+    
+    let index = 0;
+    
+    let selectOption = d3.event.target[index];
+    
+    while(selectOption != undefined){
 
-// *********************************************
-// ******  Functions to Manage the   ***********
-// ******  Data in the HTML Table    ***********
-// *********************************************
+        if (selectOption.selected === true){
+            filterValues.push(selectOption.value);
+        }
+
+        selectOption = d3.event.target[++index];
+    }
+    
+    ufoTableFilter.setFilter(d3.event.target.id, filterValues);
+
+    filterTable();
+}
+
 
 
 // Reference the data HTML table
@@ -184,43 +303,17 @@ function buildTable(dataArray) {
     });
 };
 
-function selectOnChange() {
+function filterTable(tableFilter) {
 
-    // Get the selected option values from the select 
-    // filter controls with the multiselect property
-    // set to true.
-    // Store the selected options values for each select
-    // control and store them into an array
-    let datetimeArray = getSelectOptions("datetime");
-    let cityArray     = getSelectOptions("city");
-    let stateArray    = getSelectOptions("state");
-    let countryArray  = getSelectOptions("country");
-    let shapeArray    = getSelectOptions("shape");
-
+    // Create copy of tableData
     let filteredData = tableData;
-
-    // Check to see if any select controls have one or more options selected
-    if (datetimeArray.length + cityArray.length + stateArray.length + countryArray.length + shapeArray.length > 0) {
  
-      // Filter the data in the data with the selected 
-      // option values for each filter select control
-      filteredData = filteredData.filter((row) => 
-      
-        (datetimeArray.length === 0 | datetimeArray.includes(row.datetime))
-        &
-        (cityArray.length === 0 | cityArray.includes(row.city))
-        &
-        (stateArray.length === 0 | stateArray.includes(row.state))
-        &
-        (countryArray.length === 0 | countryArray.includes(row.country))
-        &
-        (shapeArray.length === 0 | shapeArray.includes(row.shape))
-    );
-}
+    // Filter copy of table data
+    filteredData = filteredData.filter((row) => { return ufoTableFilter.filter(row) });
 
     // Rebuild the table using the filtered data
-    // @NOTE: If no select box options are selected
-    //the data in the table will just be the original tableData.
+    // @NOTE: If no select element options are selected
+    // table will contain all the rows in thetableData object.
     buildTable(filteredData);
 };
 
@@ -232,18 +325,14 @@ function selectOnChange() {
 // ******  Initialze the Webpage     ***********
 // *********************************************
 
-// Attach the select controls clear selected option function
-// to the clear filter control
-d3.selectAll("#clear-filter").on("click", clearSelectBoxes);
+// Attach the select elements clear selected option function
+// to the clear filter element
+d3.selectAll("#clear-filter").on("click", clearFilterSelects);
 
 
 // Build the table when the page loads
 buildTable(tableData);
 
-// Populate form filter select controls with 
+// Populate form filter select elements with 
 // the unique data values for each data field
-populateSelectOptions("datetime", dataFieldValues["datetime"]);
-populateSelectOptions("city",     dataFieldValues["city"], format="capitalize");
-populateSelectOptions("state",    dataFieldValues["state"], format="uppercase");
-populateSelectOptions("country",  dataFieldValues["country"], format="uppercase");
-populateSelectOptions("shape",    dataFieldValues["shape"], format="capitalize");
+populateFilterSelects(uniqSortFldVals, updateFilter); 
